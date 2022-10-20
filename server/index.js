d1c7646168f75f3e004d53c241fb4f2fc5751d60
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-10-20 17:53:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const connection = require("./db")
+require("dotenv").config();
+const PORT = process.env.PORT
 
 const app = express();
+app.use(express.json())
+
 
 app.get("/", (req,res)=>{
     res.send("Welcome to backend home")
 })
 
-app.listen(8080, (req, res)=>{
-    
+app.listen(PORT, async()=>{
+    try{
+        await connection ;
+        console.log("connected to nayadrawboard database");
+    }catch(e){
+        console.log("error connecting to database",e);
+    }
 })
