f5336a1fed2d9f1c50167fa5adaca1c1740940e0
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-10-23 00:05:08 +0530

    nayalogo updated

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from "./navbar.module.css"
+import NayaLogo from "../../collections/images/NayaLogo.png"
 
 const Navbar = () => {
   return (
     <div className={styles.parentDiv}>
         <div className={styles.imgDiv}>
-            <img src="https://media-exp1.licdn.com/dms/image/C560BAQEaxYgfOrYbVw/company-logo_200_200/0/1657135218810?e=1674086400&v=beta&t=4jECrCAqE3TqF8h3BAQyCebIB7vXNTks4QMaPLcX080" alt="NayaLogo" />
+            <img src={NayaLogo} alt="NayaLogo" />
         </div>
     </div>
   )
