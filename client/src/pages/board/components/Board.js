2e126e58f7258fb5da3d3249bcf78854311cc921
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-10-24 03:04:41 +0530

    Update Board.js

--- client/src/pages/board/components/Board.js
+++ client/src/pages/board/components/Board.js
@@ -1,25 +1,24 @@
-import React, { useState } from 'react'
-import styles from "./board.module.css"
-import {useOnDraw} from "../components/Hooks"
-import useRandomColor from './useRandomColor'
-import { useEffect } from 'react'
+import React, { useState } from "react";
+import styles from "./board.module.css";
+import { useOnDraw } from "../components/Hooks";
+import useRandomColor from "./useRandomColor";
+import { useEffect } from "react";
 
 const boardStyle = {
-    border: '1px solid black',
-    margin: 'auto'
-}
+  border: "1px solid black",
+  margin: "auto",
+};
 
-const Board = ({width, height}) => {
+const Board = ({ width, height }) => {
   const setBoardRef = useOnDraw(onDraw);
   const randomColor = useRandomColor();
 
-
   function onDraw(ctx, point, prevPoint) {
-   drawLine(prevPoint, point, ctx, randomColor , 5)
+    drawLine(prevPoint, point, ctx, randomColor, 5);
   }
 
-  function drawLine(start, end, ctx, color, width){
-    start = start ??  end;
+  function drawLine(start, end, ctx, color, width) {
+    start = start ?? end;
     ctx.beginPath();
     ctx.lineWidth = width;
     ctx.strokeStyle = color;
@@ -29,21 +28,20 @@ const Board = ({width, height}) => {
 
     ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI)
+    ctx.arc(start.x, start.y, 2, 0, 2 * Math.PI);
     ctx.fill();
   }
 
   return (
     <div className={styles.container}>
       <canvas
-      width={width}
-      height={height} 
-          style={boardStyle}
-          ref = {setBoardRef}
-          />      
+        width={width}
+        height={height}
+        style={boardStyle}
+        ref={setBoardRef}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Board
-
+export default Board;
