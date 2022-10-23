63fe061c3873b95323e73790a1d8a45e8dcb994b
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-10-24 03:04:57 +0530

    Update useRandomColor.js

--- client/src/pages/board/components/useRandomColor.js
+++ client/src/pages/board/components/useRandomColor.js
@@ -1,23 +1,21 @@
 import { useEffect } from "react";
-import { useState } from "react"
+import { useState } from "react";
 
-const useRandomColor=()=>{
+const useRandomColor = () => {
+  const [randomColor, setRandomColor] = useState("");
 
-    const [randomColor, setRandomColor ] = useState('');
+  let maxVal = 0xffffff; // 16777215.
+  let randomNumber = Math.random() * maxVal;
+  randomNumber = Math.floor(randomNumber);
+  randomNumber = randomNumber.toString(16);
+  let randColor = randomNumber.padStart(6, 0);
+  randColor = `#${randColor.toUpperCase()}`;
 
-    let maxVal = 0xFFFFFF; // 16777215.
-    let randomNumber = Math.random() * maxVal;
-    randomNumber = Math.floor(randomNumber);
-    randomNumber = randomNumber.toString(16);
-    let randColor = randomNumber.padStart(6, 0);
-    randColor = `#${randColor.toUpperCase()}`
+  useEffect(() => {
+    setRandomColor(randColor);
+  }, []);
 
-    useEffect(()=>{
-        setRandomColor(randColor)
-    }, [])
+  return randomColor;
+};
 
-     return randomColor;
-
-}
-
-export default useRandomColor
+export default useRandomColor;
