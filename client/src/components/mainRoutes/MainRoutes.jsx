2c71bc4aea78f92394499b9d224149e3ee47b7ad
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-10-24 03:07:03 +0530

    Update MainRoutes.jsx

--- client/src/components/mainRoutes/MainRoutes.jsx
+++ client/src/components/mainRoutes/MainRoutes.jsx
@@ -1,24 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Board from '../../pages/board/components/Board'
-import Login from '../../pages/login/Login'
-import SignUp from '../../pages/register/SignUp'
-import Navbar from '../navbar/Navbar'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Board from "../../pages/board/components/Board";
+import Login from "../../pages/login/Login";
+import SignUp from "../../pages/register/SignUp";
+import Navbar from "../navbar/Navbar";
 
 const MainRoutes = () => {
   return (
-
     <div>
-        <Navbar />
-            {/* <Route path="/" element={<Navbar />} /> */}
-        <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/drawboard" element={<Board width={800} height={500} />} />          
-        </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/drawboard" element={<Board width={800} height={500} />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default MainRoutes
+export default MainRoutes;
